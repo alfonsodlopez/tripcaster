4f5f9fc6a6d4ee18e2aa5b08b62d7c37c8d23bca
Alfonso Lopez <alfonsodlopez@gmail.com>
2018-01-19 02:48:09 -0800

    ListenNotes API functional. Updated layout for testing. Will add Google API in next update.

--- App.js
+++ App.js
@@ -14,24 +14,115 @@ import {
   Button,
   TextInput,
 } from 'react-native';
+import secrets from './secrets'
 
 export default class App extends Component<{}> {
 
     state = {
-        currentLocation: "",
-        destination: "",
-        searchQuery: ""
+        currentLocation: '',
+        destination: '',
+        searchQuery: '',
+        listenNotesURL: 'https://listennotes.p.mashape.com/api/v1/search?offset=&',
+        len_min: 0,
+        len_max: 200,
+        offset: 0,
+        text: '',
+        travelMode: 'DRIVING'
     }
+
+    podcastSearchURL = `${this.state.listenNotesURL}`
 
     _onPressButton() {
-        Alert.alert('You tapped the button!')
-    }
+        const params = {
+            offset: this.state.offset,
+            len_min: this.state.len_min,
+            len_max: this.state.len_max,
+            q: this.state.searchQuery 
+        };
+        const esc = encodeURIComponent;
+        const query = Object.keys(params)
+            .map(k => esc(k) + '=' + esc(params[k]))
+            .join('&');
+        console.log(this.podcastSearchURL+query)
+        console.log(secrets)
+        fetch(this.podcastSearchURL+query, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-Mashape-Key': secrets.podcasts
+            }
+        })
+        .then(res => {
+            console.log(res)
+            return res.json()
+        })
+        .catch((error) => {
+            console.log('Error', error); // no error is returned
+            throw error;
+      })
+
+    };
+
+    otherPressButton() {
+        var origin1 = new google.maps.LatLng(55.930385, -3.118425);
+        var origin2 = 'Greenwich, England';
+        var destinationA = 'Stockholm, Sweden';
+        var destinationB = new google.maps.LatLng(50.087692, 14.421150);
+
+        var service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix(
+            {
+                origins: [this.state.currentLocation],
+                destinations: [this.state.destination],
+                travelMode: this.state.travelMode,
+            }, callback);
+
+        function callback(response, status) {
+        //The DistanceMatrixResponse object contains one row for each origin 
+        //that was passed in the request. Each row contains an element field 
+        //for each pairing of that origin with the provided destination(s).
+            if (status == 'OK') {
+                var origins = response.originAddresses;
+                var destinations = response.destinationAddresses;
+
+            for (var i = 0; i < origins.length; i++) {
+                var results = response.rows[i].elements;
+                for (var j = 0; j < results.length; j++) {
+                    var element = results[j];
+                    var distance = element.distance.text;
+                    var duration = element.duration.text;
+                    var from = origins[i];
+                    var to = destinations[j];
+                    }
+                }
+                console.log(results)
+            }
+
+        }
+    };
 
     enterText(value) {
-    /*    console.log(value);*/  
+        console.log(value) 
+        this.state.text = value 
         this.setState(this.state)
     };
 
+    enterSearch(value) {
+        console.log('Search: ' + value)
+        this.state.searchQuery = value
+        this.setState(this.state)
+    };
+
+    enterLocation(value) {
+        this.state.currentLocation = value
+        this.setState(this.state)
+    };
+
+    enterDestination(value) {
+        this.state.destination = value
+        this.setState(this.state)
+    };
 
 
   render() {
@@ -43,7 +134,7 @@ export default class App extends Component<{}> {
             </Text>
             <TextInput 
                 style={styles.textInput}
-                onChangeText={this.enterText.bind(this)}
+                onChangeText={this.enterLocation.bind(this)}
             >
             </TextInput>
         </View>
@@ -53,16 +144,33 @@ export default class App extends Component<{}> {
             </Text>
             <TextInput 
                 style={styles.textInput}
-                onChangeText={this.enterText.bind(this)}
+                onChangeText={this.enterDestination.bind(this)}
+            >
+            </TextInput>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.label}>
+                What topic are you interested in?
+            </Text>
+            <TextInput 
+                style={styles.textInput}
+                onChangeText={this.enterSearch.bind(this)}
             >
             </TextInput>
         </View>
         <Button
             className = "submit_button"
-            onPress={this._onPressButton}
-            title="Submit"
+            onPress={this._onPressButton.bind(this)}
+            title="Submit for ListenNotes Response"
             color="#841584"
-            accessibilityLabel="Learn more about this purple button"
+            accessibilityLabel="Press this button to get episodes for your commute"
+        />
+        <Button
+            className = "submit_button"
+            onPress={this.otherPressButton.bind(this)}
+            title="Submit for Maps Response"
+            color="#841584"
+            accessibilityLabel="Press this button to get episodes for your commute"
         />
       </View>
     );
